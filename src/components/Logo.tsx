8df8cd657991fb6be7772b91446cb05e28186d42
성태경 <taegyeong0320@naver.com>
2025-01-14 15:41:35 +0900

    design: Logo 컴포넌트 생성

---
+++ src/components/Logo.tsx
@@ -0,0 +1,11 @@
+const Logo = () => {
+  return (
+    <div className='flex flex-col'>
+      <p className='text-focus-color'>FOCUS</p>
+      <p>Filter Out Chaos,</p>
+      <p>Unlock Success!</p>
+    </div>
+  );
+};
+
+export default Logo;
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+import Logo from '../components/Logo';
+
+const LoginPage = () => {
+  return (
+    <div className=''>
+      <Logo />
+    </div>
+  );
+};
+
+export default LoginPage;
