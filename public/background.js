b72a7fca3fc59e170e93640a24d21738cff6199f
성태경 <taegyeong0320@naver.com>
2025-01-25 23:13:57 +0900

    feat: 기능 구현

--- public/background.js
+++ public/background.js
@@ -6,3 +6,29 @@ chrome.sidePanel
 chrome.runtime.onUpdateAvailable.addListener(() => {
   chrome.runtime.reload();
 });
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  const { user_id, user_name, is_logged_in } = message;
+
+  console.log({
+    user_id,
+    user_name,
+    is_logged_in,
+  });
+
+  if (user_id && user_name && is_logged_in !== undefined) {
+    // Chrome Storage에 데이터 저장
+    chrome.storage.local.set({ user_id, user_name, is_logged_in }, () => {
+      console.log({
+        user_id,
+        user_name,
+        is_logged_in,
+      });
+    });
+
+    sendResponse({ status: 'success' });
+  } else {
+    console.log(message);
+    sendResponse({ status: 'error', message });
+  }
+});
